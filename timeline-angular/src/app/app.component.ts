cecfa6c9d737298cf60be97eaebfed4baa02297b
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2025-01-20 22:19:03 +0400

    complete angular version on timeline project, just for showcase

--- timeline-angular/src/app/app.component.ts
+++ timeline-angular/src/app/app.component.ts
@@ -1,78 +1,21 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import data from '../data/content.json'
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
-export class AppComponent{
-  title = 'timeline-angular';
-  timelineItems:any[] = [
-    {
-        "title": "პაპილომა",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
-    },
-    {
-        "title": "პაპილომა",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
-    },
-    {
-        "title": "პაპილომა",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
-    },
-    {
-        "title": "title 4",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
-    },
-    {
-        "title": "title 5",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
-    },
-    {
-        "title": "title 6",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sunt!",
-        "date": "0.5",
-        "type": "vaccine",
-        "selectionImage": "card-images/vaccine.svg",
-        "virusImage": "card-images/virus-image.png"
+export class AppComponent {
+    title = 'timeline-angular';
+    activeTab: string = 'child'
+    timelineItems: any[] = []
+    onTabClick(tab: string, event: any) {
+        this.timelineItems = data.filter((item:any) => item.for === event.target.id)
+        this.activeTab = tab
     }
-]
- 
-
-
-// @HostListener('wheel', ['$event'])
-// onWheel(event: WheelEvent): void {
-//   const timelineWrapper = document.querySelector('.timeline-wrapper') as HTMLElement;
-
-//   if (timelineWrapper) {
-//     // Scroll by delta of the wheel scroll event
-//     timelineWrapper.scrollLeft += (event.deltaY * 2); // Scroll horizontally
-//   }
-// }
-ngOnInit() {
-  // Any initialization code if needed
-}
+    ngOnInit() {
+        this.timelineItems = data.filter((item:any) => item.for === 'child')
+    }
 }
